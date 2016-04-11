a47e44d79d8fe9af6128448d2503137818a309e0
Dmitry Sorin <dsorin@yandex-team.ru>
2016-04-12 00:45:15 +0300

    Add tests for insert op

--- test/test.js
+++ test/test.js
@@ -49,6 +49,14 @@ describe('node-couchdb tests', () => {
         assert.strictEqual(couch4._cache, cache);
     });
 
+    it('should fail if CouchDB server is unavailable', () => {
+        const couch = new nodeCouchDb({port: 80});
+
+        return couch.createDatabase(dbName).then(() => {
+            throw new Error('Error was expected but nothing happened');
+        }, err => ({}));
+    });
+
     it('should replace cache object', () => {
         couch.useCache(null);
         assert.isNull(couch._cache);
@@ -60,8 +68,6 @@ describe('node-couchdb tests', () => {
     it('should create database', () => {
         const promise = couch.createDatabase(dbName);
         assert.instanceOf(promise, Promise, 'createDatabase() result is not a promise');
-
-        return promise;
     });
 
     it('should reject with EDBEXISTS is database already exists', () => {
@@ -72,6 +78,33 @@ describe('node-couchdb tests', () => {
             }, err => {
                 assert.instanceOf(err, Error, 'rejected error is not an instance of Error');
                 assert.strictEqual(err.code, 'EDBEXISTS');
+            });
+    });
+
+    it('should return promise for dropDatabase operation', () => {
+        const promise = couch.dropDatabase(dbName);
+        assert.instanceOf(promise, Promise, 'dropDatabase() result is not a promise');
+    });
+
+    it('should fail with EDBMISSING if database with this name doesn\'t exist', () => {
+        return couch.dropDatabase(dbName).then(() => {
+            throw new Error('dropDatabase() op promise resolved for missing database');
+        }, err => {
+            assert.instanceOf(err, Error, 'err is not an instance of Error');
+            assert.strictEqual(err.code, 'EDBMISSING', 'err code is not EDBMISSING');
+        });
+    });
+
+    it('should create and drop database', () => {
+        return couch.createDatabase(dbName)
+            .then(() => couch.listDatabases())
+            .then(dbs => {
+                assert.include(dbs, dbName, 'databases list doesn\'t contain created database');
+            })
+            .then(() => couch.dropDatabase(dbName))
+            .then(() => couch.listDatabases())
+            .then(dbs => {
+                assert.notInclude(dbs, dbName, 'databases list still contains created database');
             });
     });
 
@@ -92,74 +125,48 @@ describe('node-couchdb tests', () => {
             });
     });
 
+    it('listDatabase should not crash when parsing result', () => {
+        return couch.listDatabases(dbs => {
+            const types = dbs.reduce((memo, db) => {
+                const type = typeof db;
+
+                if (!memo.includes(type)) {
+                    memo.push(type);
+                }
+
+                return memo;
+            }, []);
+
+            assert.strictEqual(types.length, 1, 'More than one type is listed among dbs');
+            assert.strictEqual(types[0], 'string', 'Type is not a string');
+        });
+    });
+
+    it('should return promise for insert operation', () => {
+        const promise = couch.insert(dbName, {});
+        assert.instanceOf(promise, Promise, 'insert() result is not a promise');
+    });
+
+    it('should insert documents', () => {
+        return couch.createDatabase(dbName)
+            .then(() => couch.insert(dbName, {}))
+            .then(resData => {
+                assert.isObject(resData, 'result is not an object');
+                assert.isObject(resData.headers, 'result headers is not an object');
+                assert.isObject(resData.data, 'result data is not an object');
+                assert.isString(resData.data.id, 'ID is not a string');
+                assert.match(resData.data.id, /^[a-z0-9]+$/, 'ID is not valid');
+                assert.isNumber(resData.status, 'status is not a number')
+            });
+    });
 
 
-    // it('should create database', done => {
-    //     couch.createDatabase(dbName, err => {
-    //         assert.isNull(err, 'Unexpected error occured');
-    //         done();
-    //     });
-    // });
 
-    // it('should fail with EDBEXISTS if database exists', done => {
-    //     couch.createDatabase(dbName, err => {
-    //         assert.isNull(err, 'Unexpected error occured');
+    
 
-    //         couch.createDatabase(dbName, err => {
-    //             assert.isNotNull(err, 'Error was expected but nothing happened');
-    //             assert.instanceOf(err, Error, 'err is not an instance of Error');
-    //             assert.strictEqual(err.code, 'EDBEXISTS', 'err code is not EDBEXISTS');
+    
 
-    //             done();
-    //         });
-    //     });
-    // });
-
-    // it('should drop database', done => {
-    //     couch.createDatabase(dbName, err => {
-    //         assert.isNull(err, 'Unexpected error occured');
-
-    //         couch.dropDatabase(dbName, err => {
-    //             assert.isNull(err, 'Unexpected error occured');
-    //             done();
-    //         });
-    //     });
-    // });
-
-    // it('should fail with EDBMISSING if database with this name doesn\'t exist', done => {
-    //     couch.dropDatabase(dbName, err => {
-    //         assert.isNotNull(err, 'Error was expected but nothing happened');
-    //         assert.instanceOf(err, Error, 'err is not an instance of Error');
-    //         assert.strictEqual(err.code, 'EDBMISSING', 'err code is not EDBMISSING');
-
-    //         done();
-    //     });
-    // });
-
-    // it('should fail if CouchDB server is unavailable', done => {
-    //     let couch = new nodeCouchDb('127.0.0.2', 80);
-
-    //     couch.createDatabase(dbName, err => {
-    //         assert.isNotNull(err, 'Error was expected but nothing happened');
-    //         done();
-    //     });
-    // });
-
-    // it('should insert documents', done => {
-    //     couch.createDatabase(dbName, err => {
-    //         assert.isNull(err, 'Unexpected error occured');
-
-    //         couch.insert(dbName, {}, (err, resData) => {
-    //             assert.isNull(err, 'Unexpected error occured');
-    //             assert.isObject(resData, 'Result is not an object');
-    //             assert.isObject(resData.data, 'Result data is not an object');
-    //             assert.isString(resData.data.id, 'ID is not a string');
-    //             assert.match(resData.data.id, /^[a-z0-9]+$/, 'ID is not valid');
-
-    //             done();
-    //         });
-    //     });
-    // });
+    
 
     // it('should get expected document', done => {
     //     couch.createDatabase(dbName, err => {
@@ -183,27 +190,7 @@ describe('node-couchdb tests', () => {
     //     });
     // });
 
-    // it('listDatabase should not crash when parsing result', done => {
-    //     couch.listDatabases((err, dbs) => {
-    //         assert.isNull(err, 'Unexpected error occured');
-    //         assert.instanceOf(dbs, Array, 'dbs variable is not an Array instance');
-
-    //         const types = dbs.reduce((memo, db) => {
-    //             const type = typeof db;
-
-    //             if (!memo.includes(type)) {
-    //                 memo.push(type);
-    //             }
-
-    //             return memo;
-    //         }, []);
-
-    //         assert.strictEqual(types.length, 1, 'More than one type is listed among dbs');
-    //         assert.strictEqual(types[0], 'string', 'Type is not a string');
-
-    //         done();
-    //     });
-    // });
+    
 
     // it('should not encode startkey_docid as JSON', done => {
     //     couch.createDatabase(dbName, err => {
@@ -255,91 +242,4 @@ describe('node-couchdb tests', () => {
 // 			});
 // 		});
 // 	});
-// };
-
-// exports.cache = {
-// 	tearDown: function (callback) {
-// 		if (this.memcachedClient)
-// 			this.memcachedClient.end();
-
-// 		if (this.memcacheClient)
-// 			this.memcacheClient.close();
-
-// 		callback();
-// 	},
-
-// 	fs: function (test) {
-// 		var fs = require("fs");
-// 		var path = require("path");
-// 		var tmpDir = process.env.TMPDIR || require("os").tmpDir();
-
-// 		var fsCache = {
-// 			get: function (key, callback) {
-// 				var filePath = path.resolve(tmpDir + "/" + key);
-
-// 				fs.exists(filePath, function (exists) {
-// 					if (exists) {
-// 						fs.readFile(filePath, "utf-8", callback);
-// 					} else {
-// 						callback();
-// 					}
-// 				});
-// 			},
-
-// 			set: function (key, value, callback) {
-// 				var filePath = path.resolve(tmpDir + "/" + key);
-
-// 				fs.writeFile(filePath, value, "utf-8", function () {
-// 					callback && callback();
-// 				});
-// 			},
-
-// 			invalidate: function () {}
-// 		};
-
-// 		commonTest.call(this, test, fsCache);
-// 	},
-// 	memory: function (test) {
-// 		commonTest.call(this, test);
-// 	},
-// 	memcached: function (test) {
-// 		var memcached = require("memcached");
-// 		var memcachedClient = new memcached("localhost:11211", {keyCompression: false, timeout: 0, retries: 0, reconnect: 3000, poolSize: 1});
-
-// 		var cacheAPI = {
-// 			get: function (key, callback) {
-// 				memcachedClient.get(key, function (err, data) {
-// 					if (err || data === false || data === undefined)
-// 						return callback(err);
-
-// 					callback(null, data);
-// 				});
-// 			},
-
-// 			set: function (key, value, callback) {
-// 				memcachedClient.set(key, value, 0, function () {
-// 					callback && callback.apply(null, arguments);
-// 				});
-// 			},
-
-// 			_serialize: false
-// 		};
-
-// 		this.memcachedClient = memcachedClient;
-// 		commonTest.call(this, test, cacheAPI);
-// 	},
-// 	memcache: function (test) {
-// 		var memcache = require("memcache");
-// 		var memcacheClient = new memcache.Client(11211, "localhost");
-// 		var self = this;
-
-// 		memcacheClient.on("connect", function () {
-// 			commonTest.call(self, test, memcacheClient);
-// 		});
-
-// 		this.memcacheClient = memcacheClient;
-// 		memcacheClient.connect();
-// 	},
-
-
 // };
